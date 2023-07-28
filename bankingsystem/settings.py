7429ffd7cd0f1f0fdbf5b30a91cb1701ee9ac9e5
oragift-for-royalB <140765818+oragift-for-royalB@users.noreply.github.com>
2023-07-28 01:27:41 +0100

    Update settings.py

--- bankingsystem/settings.py
+++ bankingsystem/settings.py
@@ -169,6 +169,5 @@ AWS_S3_FILE_OVERWRITE=False
 AWS_DEFAULT_ACL=None
 AWS_S3_VERIFY=True
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
